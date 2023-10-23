9a7809f3dab1f143280b9cd878f697b8ceb54c8d
michalmjo <michal.kmjo@gmail.com>
2023-10-23 12:21:43 +0200

    sending the signup request

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from 'src/app/shared/services/language.service';
+import { AuthService } from './service/auth.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-auth',
@@ -16,8 +18,11 @@ export class AuthComponent implements OnInit {
   getSignUpText(): string {
     return this.isLoginMode ? 'Login' : 'Sign-up';
   }
+  getChangeLoginText(): string {
+    return this.isLoginMode ? 'register' : 'log-in';
+  }
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authForm = new FormGroup({
@@ -31,7 +36,31 @@ export class AuthComponent implements OnInit {
     this.isLoginMode = !this.isLoginMode;
   }
   onSubmit() {
+    if (!this.authForm.valid) {
+      return;
+    }
+
+    const email = this.authForm.get('userDataGroup.email')?.value;
+    const password = this.authForm.get('userDataGroup.password')?.value;
+
+    if (this.isLoginMode) {
+      //...
+      console.log('tautaj');
+    } else {
+      console.log('Tutaj przechodzi dalej');
+      console.log(email, password);
+      this.authService.signUp(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
     console.log(this.authForm.value);
+    console.log(this.authForm.valid!);
     this.authForm.reset();
   }
 }
